10dacc74d673b46dce76f6621e5771e14e6ec8b2
Amir Mousavi <a.mousavi@arasdp.ir>
2025-01-13 13:16:38 +0330

    test

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -6,5 +6,8 @@ const { myLayer } = useAppConfig()
   <div>
     <h1>Hello World!</h1>
     <pre>{{ myLayer }}</pre>
+    <v-container>
+      <v-btn>Hello</v-btn>
+    </v-container>
   </div>
 </template>
