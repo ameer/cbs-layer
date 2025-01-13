dd5bcc5a4de8aadb67d48f413cf0cf17ecc65496
ameer <xerxes1718@gmail.com>
2025-01-13 20:09:05 +0330

    add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { fileURLToPath } from 'url'
 import path, { dirname, join } from 'path'
+import { version } from './package.json'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   modules: ['@nuxt/eslint', 'vuetify-nuxt-module', '@sidebase/nuxt-auth'],
-  eslint: {
-    config: {
-      stylistic: true,
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      version,
+      baseURL: process.env.BASE_URL || 'http://localhost:9000',
     },
   },
   vite: {
@@ -18,10 +20,15 @@ export default defineNuxtConfig({
       },
     },
   },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
   vuetify: {
     moduleOptions: {
       /* other module options */
-      styles: { configFile: join(currentDir,'./settings.scss') },
+      styles: { configFile: join(currentDir, './settings.scss') },
     },
     vuetifyOptions: './vuetify.config.ts',
   },
